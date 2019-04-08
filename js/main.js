dc48822944358f6378444b4ffea1c29fde88bf66
Dmitriy <48007787+Eroha186@users.noreply.github.com>
2019-04-08 21:54:47 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var input = document.querySelector(".input-name");
+
+input.addEventListener("input", function() {
+  this.value = this.value[0].toUpperCase() + this.value.slice(1);
+})
+var input = document.querySelector(".popup-form__input-name");
+
+input.addEventListener("input", function() {
+  this.value = this.value[0].toUpperCase() + this.value.slice(1);
+})
+var input = document.querySelector(".question-form__input-name");
+
+input.addEventListener("input", function() {
+  this.value = this.value[0].toUpperCase() + this.value.slice(1);
+})
